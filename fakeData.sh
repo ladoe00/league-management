7f1a34a7bd7f66d4dd558d31e7216cced5eac25e
E <ladoe00@users.noreply.github.com>
2018-04-17 20:47:11 -0400

    Update fakeData script to support login and JWT token

--- fakeData.sh
+++ fakeData.sh
@@ -1,58 +1,59 @@
 #!/bin/bash
 
-read -s -p "Enter admin password:" PASSWORD
+read -s -p "Enter admin password for authentication:" PASSWORD
 echo
 # Create league
-curl -u admin:"${PASSWORD}" -X POST "http://localhost:8080/leagues?leagueName=NNHL" -H "accept: application/json"
+TOKEN=$(curl -s -u admin:"${PASSWORD}" -X GET "http://localhost:8080/auth/login" -H "accept: text/plain")
+curl -X POST "http://localhost:8080/leagues?leagueName=NNHL" -H "accept: application/json" -H "Authorization: Bearer ${TOKEN}"
 
 #
 # Create Players
 #
 # Forwards
-curl -X POST "http://localhost:8080/players?email=lemieux66%40pens.com&firstName=Mario&lastName=Lemieux&password=secret&position=FORWARD" -H "accept: application/json"
-curl -X POST "http://localhost:8080/players?email=crosby87%40pens.com&firstName=Sidney&lastName=Crosby&password=secret&position=FORWARD" -H "accept: application/json"
-curl -X POST "http://localhost:8080/players?email=thegreatone%40oilers.com&firstName=Wayne&lastName=Gretzky&password=secret&position=FORWARD" -H "accept: application/json"
-curl -X POST "http://localhost:8080/players?email=golden%40blues.com&firstName=Brett&lastName=Hull&password=secret&position=FORWARD" -H "accept: application/json"
-curl -X POST "http://localhost:8080/players?email=koivu%40habs.com&firstName=Saku&lastName=Koivu&password=secret&position=FORWARD" -H "accept: application/json"
-curl -X POST "http://localhost:8080/players?email=jotoews%40hawks.com&firstName=Jonathan&lastName=Toews&password=secret&position=FORWARD" -H "accept: application/json"
-curl -X POST "http://localhost:8080/players?email=ovy8%40caps.com&firstName=Alex&lastName=Ovechkin&password=secret&position=FORWARD" -H "accept: application/json"
-curl -X POST "http://localhost:8080/players?email=viking26%40habs.com&firstName=Mats&lastName=Naslund&password=secret&position=FORWARD" -H "accept: application/json"
-curl -X POST "http://localhost:8080/players?email=stats%40nordiques.com&firstName=Peter&lastName=Statsny&password=secret&position=FORWARD" -H "accept: application/json"
+curl -X POST "http://localhost:8080/players?email=lemieux66%40pens.com&firstName=Mario&lastName=Lemieux&password=secret&position=FORWARD" -H "accept: application/json" -H "Authorization: Bearer ${TOKEN}"
+curl -X POST "http://localhost:8080/players?email=crosby87%40pens.com&firstName=Sidney&lastName=Crosby&password=secret&position=FORWARD" -H "accept: application/json" -H "Authorization: Bearer ${TOKEN}"
+curl -X POST "http://localhost:8080/players?email=thegreatone%40oilers.com&firstName=Wayne&lastName=Gretzky&password=secret&position=FORWARD" -H "accept: application/json" -H "Authorization: Bearer ${TOKEN}"
+curl -X POST "http://localhost:8080/players?email=golden%40blues.com&firstName=Brett&lastName=Hull&password=secret&position=FORWARD" -H "accept: application/json" -H "Authorization: Bearer ${TOKEN}"
+curl -X POST "http://localhost:8080/players?email=koivu%40habs.com&firstName=Saku&lastName=Koivu&password=secret&position=FORWARD" -H "accept: application/json" -H "Authorization: Bearer ${TOKEN}"
+curl -X POST "http://localhost:8080/players?email=jotoews%40hawks.com&firstName=Jonathan&lastName=Toews&password=secret&position=FORWARD" -H "accept: application/json" -H "Authorization: Bearer ${TOKEN}"
+curl -X POST "http://localhost:8080/players?email=ovy8%40caps.com&firstName=Alex&lastName=Ovechkin&password=secret&position=FORWARD" -H "accept: application/json" -H "Authorization: Bearer ${TOKEN}"
+curl -X POST "http://localhost:8080/players?email=viking26%40habs.com&firstName=Mats&lastName=Naslund&password=secret&position=FORWARD" -H "accept: application/json" -H "Authorization: Bearer ${TOKEN}"
+curl -X POST "http://localhost:8080/players?email=stats%40nordiques.com&firstName=Peter&lastName=Statsny&password=secret&position=FORWARD" -H "accept: application/json" -H "Authorization: Bearer ${TOKEN}"
 # Defenseman
-curl -X POST "http://localhost:8080/players?email=ricky%40sens.com&firstName=Erik&lastName=Karlsson&password=secret&position=DEFENSEMAN" -H "accept: application/json"
-curl -X POST "http://localhost:8080/players?email=larry%40habs.com&firstName=Larry&lastName=Robinson&password=secret&position=DEFENSEMAN" -H "accept: application/json"
-curl -X POST "http://localhost:8080/players?email=uncledrew%40kings.com&firstName=Drew&lastName=Doughty&password=secret&position=DEFENSEMAN" -H "accept: application/json"
-curl -X POST "http://localhost:8080/players?email=pk%40preds.com&firstName=PK&lastName=Subban&password=secret&position=DEFENSEMAN" -H "accept: application/json"
+curl -X POST "http://localhost:8080/players?email=ricky%40sens.com&firstName=Erik&lastName=Karlsson&password=secret&position=DEFENSEMAN" -H "accept: application/json" -H "Authorization: Bearer ${TOKEN}"
+curl -X POST "http://localhost:8080/players?email=larry%40habs.com&firstName=Larry&lastName=Robinson&password=secret&position=DEFENSEMAN" -H "accept: application/json" -H "Authorization: Bearer ${TOKEN}"
+curl -X POST "http://localhost:8080/players?email=uncledrew%40kings.com&firstName=Drew&lastName=Doughty&password=secret&position=DEFENSEMAN" -H "accept: application/json" -H "Authorization: Bearer ${TOKEN}"
+curl -X POST "http://localhost:8080/players?email=pk%40preds.com&firstName=PK&lastName=Subban&password=secret&position=DEFENSEMAN" -H "accept: application/json" -H "Authorization: Bearer ${TOKEN}"
 # Goalies
-curl -X POST "http://localhost:8080/players?email=onegoalie%40dfd.com&firstName=Carey&lastName=Price&password=secret&position=GOALIE" -H "accept: application/json"
-curl -X POST "http://localhost:8080/players?email=themask%40dfd.com&firstName=Jacques&lastName=Plante&password=secret&position=GOALIE" -H "accept: application/json"
-curl -X POST "http://localhost:8080/players?email=eddy%40dfd.com&firstName=Ed&lastName=Belfour&password=secret&position=GOALIE" -H "accept: application/json"
-curl -X POST "http://localhost:8080/players?email=martybro%40dfd.com&firstName=Martin&lastName=Brodeur&password=secret&position=GOALIE" -H "accept: application/json"
+curl -X POST "http://localhost:8080/players?email=onegoalie%40dfd.com&firstName=Carey&lastName=Price&password=secret&position=GOALIE" -H "accept: application/json" -H "Authorization: Bearer ${TOKEN}"
+curl -X POST "http://localhost:8080/players?email=themask%40dfd.com&firstName=Jacques&lastName=Plante&password=secret&position=GOALIE" -H "accept: application/json" -H "Authorization: Bearer ${TOKEN}"
+curl -X POST "http://localhost:8080/players?email=eddy%40dfd.com&firstName=Ed&lastName=Belfour&password=secret&position=GOALIE" -H "accept: application/json" -H "Authorization: Bearer ${TOKEN}"
+curl -X POST "http://localhost:8080/players?email=martybro%40dfd.com&firstName=Martin&lastName=Brodeur&password=secret&position=GOALIE" -H "accept: application/json" -H "Authorization: Bearer ${TOKEN}"
 
 #
 # Subscription
 #
-curl -u admin:"${PASSWORD}" -X POST "http://localhost:8080/leagues/1/players/2?subscription=REGULAR" -H "accept: application/json"
-curl -u admin:"${PASSWORD}" -X POST "http://localhost:8080/leagues/1/players/3?subscription=REGULAR" -H "accept: application/json"
-curl -u admin:"${PASSWORD}" -X POST "http://localhost:8080/leagues/1/players/4?subscription=REGULAR" -H "accept: application/json"
-curl -u admin:"${PASSWORD}" -X POST "http://localhost:8080/leagues/1/players/5?subscription=REGULAR" -H "accept: application/json"
-curl -u admin:"${PASSWORD}" -X POST "http://localhost:8080/leagues/1/players/6?subscription=REGULAR" -H "accept: application/json"
-curl -u admin:"${PASSWORD}" -X POST "http://localhost:8080/leagues/1/players/7?subscription=SPARE" -H "accept: application/json"
-curl -u admin:"${PASSWORD}" -X POST "http://localhost:8080/leagues/1/players/8?subscription=SPARE" -H "accept: application/json"
-curl -u admin:"${PASSWORD}" -X POST "http://localhost:8080/leagues/1/players/9?subscription=SPARE" -H "accept: application/json"
-curl -u admin:"${PASSWORD}" -X POST "http://localhost:8080/leagues/1/players/10?subscription=REGULAR" -H "accept: application/json"
-curl -u admin:"${PASSWORD}" -X POST "http://localhost:8080/leagues/1/players/11?subscription=REGULAR" -H "accept: application/json"
-curl -u admin:"${PASSWORD}" -X POST "http://localhost:8080/leagues/1/players/12?subscription=REGULAR" -H "accept: application/json"
-curl -u admin:"${PASSWORD}" -X POST "http://localhost:8080/leagues/1/players/13?subscription=SPARE" -H "accept: application/json"
-curl -u admin:"${PASSWORD}" -X POST "http://localhost:8080/leagues/1/players/14?subscription=REGULAR" -H "accept: application/json"
-curl -u admin:"${PASSWORD}" -X POST "http://localhost:8080/leagues/1/players/15?subscription=REGULAR" -H "accept: application/json"
-curl -u admin:"${PASSWORD}" -X POST "http://localhost:8080/leagues/1/players/16?subscription=SPARE" -H "accept: application/json"
-curl -u admin:"${PASSWORD}" -X POST "http://localhost:8080/leagues/1/players/17?subscription=SPARE" -H "accept: application/json"
-curl -u admin:"${PASSWORD}" -X POST "http://localhost:8080/leagues/1/players/18?subscription=SPARE" -H "accept: application/json"
+curl -u admin:"${PASSWORD}" -X POST "http://localhost:8080/leagues/1/players/2?subscription=REGULAR" -H "accept: application/json" -H "Authorization: Bearer ${TOKEN}"
+curl -u admin:"${PASSWORD}" -X POST "http://localhost:8080/leagues/1/players/3?subscription=REGULAR" -H "accept: application/json" -H "Authorization: Bearer ${TOKEN}"
+curl -u admin:"${PASSWORD}" -X POST "http://localhost:8080/leagues/1/players/4?subscription=REGULAR" -H "accept: application/json" -H "Authorization: Bearer ${TOKEN}"
+curl -u admin:"${PASSWORD}" -X POST "http://localhost:8080/leagues/1/players/5?subscription=REGULAR" -H "accept: application/json" -H "Authorization: Bearer ${TOKEN}"
+curl -u admin:"${PASSWORD}" -X POST "http://localhost:8080/leagues/1/players/6?subscription=REGULAR" -H "accept: application/json" -H "Authorization: Bearer ${TOKEN}"
+curl -u admin:"${PASSWORD}" -X POST "http://localhost:8080/leagues/1/players/7?subscription=SPARE" -H "accept: application/json" -H "Authorization: Bearer ${TOKEN}"
+curl -u admin:"${PASSWORD}" -X POST "http://localhost:8080/leagues/1/players/8?subscription=SPARE" -H "accept: application/json" -H "Authorization: Bearer ${TOKEN}"
+curl -u admin:"${PASSWORD}" -X POST "http://localhost:8080/leagues/1/players/9?subscription=SPARE" -H "accept: application/json" -H "Authorization: Bearer ${TOKEN}"
+curl -u admin:"${PASSWORD}" -X POST "http://localhost:8080/leagues/1/players/10?subscription=REGULAR" -H "accept: application/json" -H "Authorization: Bearer ${TOKEN}"
+curl -u admin:"${PASSWORD}" -X POST "http://localhost:8080/leagues/1/players/11?subscription=REGULAR" -H "accept: application/json" -H "Authorization: Bearer ${TOKEN}"
+curl -u admin:"${PASSWORD}" -X POST "http://localhost:8080/leagues/1/players/12?subscription=REGULAR" -H "accept: application/json" -H "Authorization: Bearer ${TOKEN}"
+curl -u admin:"${PASSWORD}" -X POST "http://localhost:8080/leagues/1/players/13?subscription=SPARE" -H "accept: application/json" -H "Authorization: Bearer ${TOKEN}"
+curl -u admin:"${PASSWORD}" -X POST "http://localhost:8080/leagues/1/players/14?subscription=REGULAR" -H "accept: application/json" -H "Authorization: Bearer ${TOKEN}"
+curl -u admin:"${PASSWORD}" -X POST "http://localhost:8080/leagues/1/players/15?subscription=REGULAR" -H "accept: application/json" -H "Authorization: Bearer ${TOKEN}"
+curl -u admin:"${PASSWORD}" -X POST "http://localhost:8080/leagues/1/players/16?subscription=SPARE" -H "accept: application/json" -H "Authorization: Bearer ${TOKEN}"
+curl -u admin:"${PASSWORD}" -X POST "http://localhost:8080/leagues/1/players/17?subscription=SPARE" -H "accept: application/json" -H "Authorization: Bearer ${TOKEN}"
+curl -u admin:"${PASSWORD}" -X POST "http://localhost:8080/leagues/1/players/18?subscription=SPARE" -H "accept: application/json" -H "Authorization: Bearer ${TOKEN}"
 
 #
 # Start new season
 #
-curl -u admin:"${PASSWORD}" -X POST "http://localhost:8080/games/newseason?leagueId=1&startDate=2018-01-01&numberOfGames=10" -H "accept: application/json"
+curl -u admin:"${PASSWORD}" -X POST "http://localhost:8080/games/newseason?leagueId=1&startDate=2018-01-01&numberOfGames=10" -H "accept: application/json" -H "Authorization: Bearer ${TOKEN}"
 
 echo
